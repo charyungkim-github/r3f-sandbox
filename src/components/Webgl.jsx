bfd0fbbc30753596a3554633fb7bce6c23889a27
charyungkim-github <58928560+charyungkim-github@users.noreply.github.com>
2024-04-05 09:33:45 +0900

    clean up scene

--- src/components/Webgl.jsx
+++ src/components/Webgl.jsx
@@ -1,14 +1,13 @@
 import { Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
+
 import Scene from './Webgl/Scene'
-import { Perf } from 'r3f-perf'
 
 export default function Webgl() {
   return (
     <div className='glContainer'>
       <Canvas gl={{ alpha: false }} shadows>
         <Suspense>
-          <Perf/>
           <Scene />
         </Suspense>
       </Canvas>
--- src/components/Webgl/Scene.jsx
+++ src/components/Webgl/Scene.jsx
@@ -8,22 +8,36 @@ import Floor from './Floor'
 export default function Scene() {
 
   const cameraRef = useRef()
+
+  return (
+    <>
+      {/* Props */}
+      <Cube />
+      <Floor />
+
+      {/* Lights */}
+      <directionalLight castShadow intensity={1} position={[-10, 20, 5]} color={'#ffffff'}/>
+      <ambientLight intensity={1} color={"#e0b270"} />
+      <color attach={"background"} color={'#fff'} />
+
+      {/* Camera */}
+      <PerspectiveCamera ref={cameraRef} position={[-3, 15, 10.5]} makeDefault />
+      <OrbitControls />
+
+      {/* Others */}
+      <KeyEvents cameraRef={cameraRef} />
+      <Perf />
+    </>
+  )
+}
+
+function KeyEvents(props) {
+
   useEffect(()=> {
-    const onKeyDown = (e) => (e.key == "t") && console.log(cameraRef.current.position)
+    const onKeyDown = (e) => (e.key == "t") && console.log(props.cameraRef.current.position)
     document.addEventListener('keydown', onKeyDown)
     return() => document.removeEventListener('keydown', onKeyDown)
   }, [])
 
-  return (
-    <>
-      <Cube />
-      <Floor />
-      <directionalLight castShadow intensity={1} position={[-10, 20, 5]} color={'#ffffff'}/>
-      <ambientLight intensity={1} color={"#e0b270"} />
-      <PerspectiveCamera ref={cameraRef} position={[-3, 15, 10.5]} makeDefault />
-      <OrbitControls />
-      <color attach='background' args={['#ffffff']} />
-      <Perf />
-    </>
-  )
+  return null
 }
